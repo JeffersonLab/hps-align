68058490cae934f517a449458e994c1437e572f6
tomeichlersmith <eichl008@umn.edu>
2023-05-30 09:51:27 -0500

    update description

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description="HPS Alignment",  # Optional
+    description="HPS Alignment plotting and tools",  # Optional
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
     #
