12fb4a46a5b7e7b0ee3cdbb7b3841ee1b8efb3fa
tomeichlersmith <eichl008@umn.edu>
2023-06-22 09:03:01 -0500

    drop JSON reading from detdump.plot._load

--- src/hps_align/detdump/plot/_load.py
+++ src/hps_align/detdump/plot/_load.py
@@ -5,38 +5,6 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
-from .._write import OutputType
-
-def _read_multitype(f: Path):
-    """Load the input file from any of the supported detdump output types
-
-    Parameters
-    ----------
-    f : str | Path
-      file path to file with detector dump
-
-    Returns
-    -------
-    pd.DataFrame
-        dataframe loaded from that file
-    """
-
-    if not isinstance(f, Path):
-        f = Path(f)
-
-    if not f.is_file():
-        raise ValueError(f'File {f} does not exist.')
-
-    if not OutputType.valid(f):
-        raise ValueError(f'File {f} does not have a supported extension.')
-
-    if f.suffix == '.csv':
-        return pd.read_csv(f)
-    elif f.suffix == '.json':
-        return pd.read_json(f).transpose()
-    else:
-        return NotImplemented
-
 
 def _global(f: Path):
     r"""transform input global detdump into in-memory data table
@@ -50,21 +18,12 @@ def _global(f: Path):
         \tan\theta_z = \frac{u_y}{u_x}
 
     """
-    df = _read_multitype(f)
-    meas_cols = ['hps_position','svt_position','u','v','w']
-    for meas in meas_cols:
-        df[[f'{meas}x',f'{meas}y',f'{meas}z']] = df[meas].tolist()
-    df.rename(
-        columns = lambda colname : colname[:3]+colname[-1] if 'position' in colname and colname != 'hps_position' and colname != 'svt_position' else colname,
-        inplace=True
-    )
-    df.drop(columns = meas_cols, inplace=True)
+    df = pd.read_csv(f)
 
     df['thetax'] = np.arctan2(df.vz, df.wz)
     df['thetay'] = -np.arcsin(df.uz)
     df['thetaz'] = np.arctan2(df.uy, df.ux)
 
-    df.reset_index(names='sensor', inplace=True)
     df.drop(
         df[df.sensor.str.contains('ECalScoring')].index,
         inplace=True
@@ -88,7 +47,7 @@ def _local(f: Path):
     for later plot categorization.
     """
 
-    df = _read_multitype(f)
+    df = pd.read_csv(f)
     df['value'] = df.value.apply(pd.eval)
     df['t_r'] = (df.parameter % 10000) // 1000
     df['u_v_w'] = (df.parameter % 1000) // 100
