24040be89b2538f574813b971802d68a0be854f4
tomeichlersmith <eichl008@umn.edu>
2023-06-22 08:43:50 -0500

    remove json as output type dump option
    the two output types were there to try to help users develop their own
    plotting tools, now that we are unifying around this plotting tool we
    can drop support for the json dump and focus on the dump option that
    works best for our tools

--- src/hps_align/detdump/_write.py
+++ src/hps_align/detdump/_write.py
@@ -5,32 +5,6 @@ import json
 import csv
 
 
-class OutputType(Enum):
-    CSV = "csv"
-    JSON = "json"
-
-    def valid(p: Path):
-        """validate that the input path is one of our types
-
-        We check validity by making sure the extension of the passed
-        Path is one of the supported output types in this Enum.
-
-        Parameters
-        ----------
-        p : Path
-            path to file to check output type of
-
-        Returns
-        -------
-        bool :
-            True if valid, False otherwise
-        """
-
-        if p.suffix not in ['.'+e.value for e in OutputType.__members__.values()]:
-            return False
-        return True
-
-
 def __get_row_default(k, v):
     """default implementation of getrow"""
     return [str(k), str(v)]
@@ -50,20 +24,13 @@ def write_mapping(output_file, data, *, header=None, getrow=__get_row_default):
         list of header rows to write to csv
         no header written if not provided
     getrow: callable
-        produce a row given a key, val pair from the mapping as arguments
-        default is to just write the key and value as two columns
+        produce a row given a key, val pair from the mapping as arguments.
+        The default is to just write the key and value as two columns
+        :meth:`__get_row_default`
     """
-    if output_file.endswith('csv'):
-        if getrow is None:
-            raise ValueError('getrow necessary if attempting to write to CSV')
-        with open(output_file, 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            if header is not None:
-                csvwriter.writerow(header)
-            for name, val in data.items():
-                csvwriter.writerow(getrow(name, val))
-    elif output_file.endswith('json'):
-        with open(output_file, 'w', encoding='utf-8') as f:
-            json.dump(data, f, indent=4)
-    else:
-        raise ValueError(f'Unable to deduce output type from extension of {output_file}')
+    with open(output_file, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        if header is not None:
+            csvwriter.writerow(header)
+        for name, val in data.items():
+            csvwriter.writerow(getrow(name, val))
