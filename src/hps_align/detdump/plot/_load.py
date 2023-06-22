d1c0dcd114ec8fa1de0dd7b6a7633500ed645452
Cameron Bravo <bravo@slac.stanford.edu>
2023-06-21 18:19:41 -0700

    Reorder the sensors on the x-axis of the plots

--- src/hps_align/detdump/plot/_load.py
+++ src/hps_align/detdump/plot/_load.py
@@ -58,7 +58,7 @@ def _global(f: Path):
         columns = lambda colname : colname[:3]+colname[-1] if 'position' in colname and colname != 'position' else colname,
         inplace=True
     )
-    df.drop(columns = meas_cols, inplace=True)
+    #df.drop(columns = meas_cols, inplace=True)
 
     df['thetax'] = np.arctan2(df.vz, df.wz)
     df['thetay'] = -np.arcsin(df.uz)
@@ -72,6 +72,12 @@ def _global(f: Path):
 
     # shorten sensor name
     df['sensor'] = df.sensor.apply(lambda s: s.replace('module0_','').replace('_sensor0','').replace('halfmodule_','').replace('module_',''))
+    df['lay'] = df.sensor.apply(lambda s: int(s[1]))
+    df['vol'] = df.sensor.apply(lambda s: 1.0 if s[2] == 'b' else 0.0)
+    df['tilt'] = df.sensor.apply(lambda s: 1.0 if (s[2] == 'b' and s[4] == 'a') or (s[2] == 't' and s[4] == 's') else 0.0)
+    df['side'] = df.sensor.apply(lambda s: 1.0 if int(s[1]) > 4 and s[-1] == 't' else 0.0)
+    df['sortval'] = 500.0*df.vol + 2.0*df.tilt + 1.0*df.side + 4.0*df.lay
+    df = df.sort_values('sortval')
     return df
 
 
